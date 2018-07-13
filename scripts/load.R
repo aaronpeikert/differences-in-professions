89a01deba8e0ab62f971d622fc59b827878f36ab
Aaron Peikert <aaron.peikert@posteo.de>
2018-07-13 11:16:09 +0200

    remove data2

--- scripts/load.R
+++ scripts/load.R
@@ -1,6 +1,5 @@
 library(tidyverse)
 library(here)
 #----load----
-data1 <- haven::read_sav(here("data", "Diplomarbeit_Welz_2011.sav"))
-data2 <- haven::read_sav(here("data", "Provadis.sav"))
-data1 <- rename(data1, gesamt = gesamtbeurteilung_mean_gerundet_je_mehr_desto_mehr)
+data <- haven::read_sav(here("data", "Diplomarbeit_Welz_2011.sav"))
+data <- rename(data, gesamt = gesamtbeurteilung_mean_gerundet_je_mehr_desto_mehr)
