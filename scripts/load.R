e50008f064424d111271e4e95a3e93bfea92719d
Aaron Peikert <aaron.peikert@posteo.de>
2018-07-13 11:23:19 +0200

    move predictors to load script

--- scripts/load.R
+++ scripts/load.R
@@ -3,3 +3,13 @@ library(here)
 #----load----
 data <- haven::read_sav(here("data", "Diplomarbeit_Welz_2011.sav"))
 data <- rename(data, gesamt = gesamtbeurteilung_mean_gerundet_je_mehr_desto_mehr)
+#----predictors----
+OCEAN <- c("O", "C", "E", "A", "N")
+predictors <- c("Berufsgruppe",
+                "verbale_Intelligenz_HIT",
+                "numerische_Intelligenz_HIT",
+                "Diktat",
+                paste0(OCEAN, rep(1:6, length(OCEAN)))) # Personality Facets
+
+model_formula <- as.formula(paste0("gesamt ~ ", paste(predictors, collapse = " + ")))
+
