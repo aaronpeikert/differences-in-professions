6e45784d5bb3e72fd64ae56d4a24f3a943b358fa
Aaron Peikert <aaron.peikert@posteo.de>
2018-07-14 09:32:00 +0200

    add fit without profession

--- scripts/02preprocess.R
+++ scripts/02preprocess.R
@@ -3,16 +3,6 @@ library(recipes)
 library(rsample)
 source(here("scripts", "01load.R"))
 
-#----recipe----
-recipe <- recipe(model_formula, 
-                 data = data) %>%
-  # Dummy variables on the qualitative predictors
-  step_dummy(all_nominal()) %>%
-  step_BoxCox(Diktat) %>% #Diktat is extreme heavy tailed
-  # Normalize
-  step_center(all_predictors()) %>%
-  step_scale(all_predictors())
-
 #----resample----
 set.seed(1002)
 data_cv <- rsample::vfold_cv(data,
@@ -20,9 +10,34 @@ data_cv <- rsample::vfold_cv(data,
                              repeats = 10, #ten repeats
                              strata = "Berufsgruppe") #stratified
 
+#----recipe-with-profession----
+recipe <- recipe(model_formula, 
+                 data = data) %>%
+  step_dummy(Berufsgruppe) %>% 
+  step_BoxCox(Diktat) %>% #Diktat is skewd/heavy tailed
+  # Normalize
+  step_center(all_predictors()) %>%
+  step_scale(all_predictors())
+
 data_cv <- mutate(data_cv, recipes = map(splits,
                                 prepper,
                                 recipe = recipe,
                                 retain = TRUE,
                                 verbose = FALSE))
 
+#----recipe-without-profession----
+predictors <- predictors[-1]
+model_formula <- as.formula(paste0("gesamt ~ ", paste(predictors, collapse = " + ")))
+
+recipe <- recipe(model_formula, 
+                 data = data) %>%
+  step_BoxCox(Diktat) %>% #Diktat is skewd/heavy tailed
+  # Normalize
+  step_center(all_predictors()) %>%
+  step_scale(all_predictors())
+
+data_cv <- mutate(data_cv, recipes = map(splits,
+                                         prepper,
+                                         recipe = recipe,
+                                         retain = TRUE,
+                                         verbose = FALSE))
