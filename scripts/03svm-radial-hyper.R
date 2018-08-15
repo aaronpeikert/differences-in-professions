44e9fb5a5759487059c9987ad0a9ce47f519f6b3
Aaron Peikert <aaron.peikert@posteo.de>
2018-08-15 08:57:15 +0200

    align the data structure of the hyper version more closly with the non hyper version

--- scripts/03svm-radial-hyper.R
+++ scripts/03svm-radial-hyper.R
@@ -3,42 +3,44 @@ library(here)
 source(here("scripts", "02preprocess.R"))
 
 #----svm-radial----
-fit_svmr <- function(recipe, hypermat, ...){
-  x <- juice(recipe, all_predictors(), composition = "data.frame")
-  y <- as.vector(juice(recipe, all_outcomes(), composition = "matrix"))
-  svm_ <- function(...)e1071::svm(x, y, ...)
-  out <- pmap(hypermat, svm_)
-  return(out)
+fit_svmr <- function(recipe, ...){
+  e1071::svm(x = juice(recipe, all_predictors(), composition = "data.frame"),
+             y = juice(recipe, all_outcomes(), composition = "matrix"),
+             ...)
 }
 hypermat <- as.data.frame(expand.grid(cost = 10^seq(-10, -1),
-                        gamma = 10^seq(-10, -1),
-                        kernel = "radial",
-                        scale = F,
-                        stringsAsFactors = FALSE))
-data_cv <- mutate(data_cv, svmr_models = map(recipes, fit_svmr, hypermat))
+                                      gamma = 10^seq(-10, -1),
+                                      kernel = "radial",
+                                      scale = F,
+                                      stringsAsFactors = FALSE))
 
-eval_fit_svmr <- function(split, recipe, models, ...) {
+data_cv <- data_cv %>%
+  mutate(hypermat = list(hypermat)) %>% 
+  unnest(hypermat, .preserve = c(splits, recipes))
+
+data_cv <- mutate(data_cv,
+                  svmr_models = pmap(list(recipe = recipes,
+                                          cost = cost,
+                                          gamma = gamma,
+                                          kernal = kernel,
+                                          scale = scale),
+                                     fit_svmr))
+
+pred_svmr <- function(split, recipe, model, ...) {
   mod_data <- bake(
     recipe, 
     newdata = assessment(split),
     all_predictors()
   )
-  true <- bake(recipe, newdata = assessment(split), all_outcomes())
-  predicted <- map(models, ~predict(.x, newdata = mod_data))
-  rmse <- map_dbl(predicted, ~sqrt(mean((true - .x)^2)))
-  r2 <- map_dbl(predicted,
-                ~cor(true, .x))^2
-  out <- data.frame(r2,
-                    rmse,
-                    cost = map_dbl(models, "cost"),
-                    degree = map_dbl(models, "degree"),
-                    gamma = map_dbl(models, "gamma"),
-                    coef0 = map_dbl(models, "coef0"),
-                    nu = map_dbl(models, "nu"),
-                    epsilon = map_dbl(models, "epsilon"))
+  out <- bake(recipe, newdata = assessment(split), all_outcomes())
+  out$predicted <- predict(model, newdata = mod_data, ...)
+  names(out)[1] <- "true"
   return(out)
 }
-
 data_cv <- mutate(data_cv,
-                  fit_svmr = pmap(list(splits, recipes, svmr_models),
-                                          eval_fit_svmr))
+                  predicted_svmr = pmap(list(splits, recipes, svmr_models),
+                                        pred_svmr),
+                  rmse_svmr = map_dbl(predicted_svmr,
+                                      ~sqrt(mean((.x$true - .x$predicted)^2))),
+                  r2_svmr = map_dbl(predicted_svmr,
+                                    ~cor(.x$true, .x$predicted))^2)
