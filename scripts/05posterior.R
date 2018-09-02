43bc564e5ca342c45b4f6bc515464a48f1b9b77f
Aaron Peikert <aaron.peikert@posteo.de>
2018-09-02 12:17:59 +0200

    add cramer v, remove plots

--- scripts/05posterior.R
+++ scripts/05posterior.R
@@ -2,25 +2,16 @@ library(here)
 library(tidyposterior)
 source(here("scripts", "04measures.R"))
 
-#----acc_model----
+#----acc-model----
 acc <- select(data_cv, starts_with("id"), starts_with("acc"))
 acc_stacked <- gather(acc)
-ggplot(acc_stacked, aes(x = model, y = statistic, group = id, col = id)) + 
-  geom_line(alpha = .75) + 
-  theme_minimal() +
-  theme(legend.position = "none") +
-  NULL
 acc_model <- perf_mod(acc)
-acc_model$stan
-
 acc_post <- tidy(acc_model)
-ggplot(acc_post) + 
-  # Add the observed data to check for consistency 
-  geom_point(
-    data = acc_stacked, 
-    aes(x = model, y = statistic), 
-    alpha = .5, col = "blue"
-  )
-
 acc_vs <- contrast_models(acc_model)
-summary(acc_vs)
+
+#----cramerv-model----
+cramerv <- select(data_cv, starts_with("id"), starts_with("CramerV"))
+cramerv_stacked <- gather(cramerv)
+cramerv_model <- perf_mod(cramerv)
+cramerv_post <- tidy(cramerv_model)
+cramerv_vs <- contrast_models(cramerv_model)
