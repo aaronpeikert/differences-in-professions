5cb69f09476766a8395c7e0ddde05474bd0fd228
Aaron Peikert <aaron.peikert@posteo.de>
2018-09-03 16:36:12 +0200

    translate variable names

--- scripts/01load.R
+++ scripts/01load.R
@@ -9,15 +9,21 @@ data <- mutate_all(data, as.vector) # remove spss atributes, they make some prob
 data <- mutate(data,
                Berufsgruppe = as.factor(Berufsgruppe),
                gesamt = as.numeric(.data$gesamt))
+data <- rename(data,
+               age = alter,
+               profession = Berufsgruppe,
+               verbal_intelligence = verbale_Intelligenz_HIT,
+               numeric_intelligence = numerische_Intelligenz_HIT,
+               rating = gesamt)
 
 #----predictors----
 OCEAN <- c("O", "C", "E", "A", "N")
-predictors_i <- c("alter",
-                "Berufsgruppe",
-                "verbale_Intelligenz_HIT",
-                "numerische_Intelligenz_HIT",
+predictors_i <- c("age",
+                "profession",
+                "verbal_intelligence",
+                "numeric_intelligence",
                 #"Diktat", # even though diktat was box-cox tranformed, centered and scaled it lowers the performance of svm to zero
                 paste0(OCEAN, rep(1:6, length(OCEAN)))) # Personality Facets
 predictors_e <- predictors_i[-2]
-model_formula_i <- as.formula(paste0("gesamt ~ ", paste(predictors_i, collapse = " + ")))
-model_formula_e <- as.formula(paste0("gesamt ~ ", paste(predictors_e, collapse = " + ")))
+model_formula_i <- as.formula(paste0("rating ~ ", paste(predictors_i, collapse = " + ")))
+model_formula_e <- as.formula(paste0("rating ~ ", paste(predictors_e, collapse = " + ")))
--- scripts/02preprocess.R
+++ scripts/02preprocess.R
@@ -8,15 +8,15 @@ set.seed(1002)
 data_cv <- rsample::vfold_cv(data,
                              v = 10, #ten folds
                              repeats = 10, #ten repeats
-                             strata = "gesamt") #stratified
+                             strata = "rating") #stratified
 
 #----recipe----
 recipe_i <- recipe(model_formula_i, 
                  data = data) %>%
   #step_BoxCox(Diktat) %>% #Diktat is skewd/heavy tailed
   # Normalize
-  step_center(all_predictors(), -Berufsgruppe) %>%
-  step_scale(all_predictors(), -Berufsgruppe)
+  step_center(all_predictors(), -profession) %>%
+  step_scale(all_predictors(), -profession)
 
 recipe_e <- recipe(model_formula_e, 
                   data = data) %>%
