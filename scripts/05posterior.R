6f69115a5424a3228cda8b241690a2483dea279a
Aaron Peikert <aaron.peikert@posteo.de>
2018-09-03 15:33:15 +0200

    add more explanations

--- scripts/05posterior.R
+++ scripts/05posterior.R
@@ -2,16 +2,20 @@ library(here)
 library(tidyposterior)
 source(here("scripts", "04measures.R"))
 
-#----acc-model----
+#----acc-stacked----
 acc <- select(data_cv, starts_with("id"), starts_with("acc"))
 acc_stacked <- gather(acc)
+
+#----acc-model----
 acc_model <- perf_mod(acc)
 acc_post <- tidy(acc_model)
 acc_vs <- contrast_models(acc_model)
 
-#----cramerv-model----
+#----cramerv-stacked----
 cramerv <- select(data_cv, starts_with("id"), starts_with("cramerv"))
 cramerv_stacked <- gather(cramerv)
+
+#----cramerv-model----
 cramerv_model <- perf_mod(cramerv)
 cramerv_post <- tidy(cramerv_model)
 cramerv_vs <- contrast_models(cramerv_model)
